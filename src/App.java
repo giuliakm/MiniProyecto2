8650caad9e9c3f6b922ad725e92c2b6eab895be9
Giuliana KM <giuliakm@example.com>
2026-03-30 10:01:37 -0500

    Crear el menú principal en consola.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Refugio refugio = new Refugio();
+
+        int opcion = 0;
+
+        do {
+
+            System.out.println("\n--- REFUGIO DE ANIMALES ---");
+            System.out.println("1. Registrar perro");
+            System.out.println("2. Registrar gato");
+            System.out.println("3. Mostrar animales");
+            System.out.println("4. Buscar animal por id");
+            System.out.println("5. Cambiar estado de un animal");
+            System.out.println("6. Ejecutar sonido de un animal");
+            System.out.println("7. Salir");
+
+        } while (opcion != 7);
+
+        sc.close();
     }
 }
