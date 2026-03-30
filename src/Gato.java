241affd1d17c4e572ff3b2dfcdefb9425fb83bc2
Giuliana KM <giuliakm@example.com>
2026-03-30 09:50:45 -0500

    Crear las clases hijas, modificar el sonido de cada animal.

--- src/Gato.java
+++ src/Gato.java
@@ -1,3 +1,11 @@
-public class Gato {
-    
+public class Gato extends Animal {
+
+    public Gato(int id, String nombre, int edad, String estado) {
+        super(id, nombre, edad, estado);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println("El gato maulla: Miau Miau");
+    }
 }
