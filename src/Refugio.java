f1cb0577d4e2a9f0ac92db4ab1f498e13343c168
Giuliana KM <giuliakm@example.com>
2026-03-30 09:55:07 -0500

    Crear la clase refugio con ArrayList, es decir, el registro de los animales.

--- src/Refugio.java
+++ src/Refugio.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+
 public class Refugio {
-    
+
+    private ArrayList<Animal> animales;
+
+    public Refugio() {
+        animales = new ArrayList<>();
+    }
+
+    public void registrarPerro(int id, String nombre, int edad) {
+
+        if (edad < 0) {
+            System.out.println("Edad inválida");
+            return;
+        }
+
+        Perro perro = new Perro(id, nombre, edad, "ACTIVO");
+        animales.add(perro);
+
+        System.out.println("Perro registrado correctamente");
+    }
+
+
+    public void registrarGato(int id, String nombre, int edad) {
+
+        if (edad < 0) {
+            System.out.println("Edad inválida");
+            return;
+        }
+
+        Gato gato = new Gato(id, nombre, edad, "ACTIVO");
+        animales.add(gato);
+
+        System.out.println("Gato registrado correctamente");
+    }
+
 }
